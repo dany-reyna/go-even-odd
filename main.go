b5f986dd85223616d4c5d4e2977718cddcb965c9
Dany Reyna <luisd.reyna@outlook.com>
2019-08-11 21:30:55 -0500

    Even odd

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func makeIntSeq(min int, max int) []int {
+	a := make([]int, max-min+1)
+	for i := range a {
+		a[i] = min + i
+	}
+	return a
+}
+
+func main() {
+	numbers := makeIntSeq(0, 10)
+
+	for _, number := range numbers {
+		if number%2 == 0 {
+			fmt.Println(number, "is even")
+		} else {
+			fmt.Println(number, "is odd")
+		}
+	}
+}
